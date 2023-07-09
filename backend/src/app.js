ca15f9ae226a7897f3aed57b6dc55f813e6ca5f4
Javier Villatoro <wemakesoftx@gmail.com>
2023-07-08 18:56:50 -0600

    feat: Crear el servidor

---
+++ backend/src/app.js
@@ -0,0 +1,37 @@
+//Requerir express
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+//require('./db.js')
+const URLORIGIN = process.env.URLORIGIN;
+
+const server = express();
+
+server.name = 'API';
+
+server.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
+server.use(bodyParser.json()); // parse application/json
+server.use(morgan('dev'));
+/**
+ * Permitir multiples origenes en la peticion hacia la API.
+ */
+server.use((req, res, next) => { 
+    const allowedOrigins = ['http://localhost:3000', URLORIGIN];
+    let origin = req.headers['origin'] || '';
+    if (allowedOrigins && allowedOrigins.includes(origin)){
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    };
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Authorization, Accept');
+    res.header('Access-Control-Allow-Credentials', true);
+    return next();
+});
+
+//Si ocurre algun error tomarlo acá, procesar el stack y mostrarlo.
+server.use((err, req, res, next) => {
+    console.error(`Error ${res.statusCode || 500}:`, err.stack);
+    res.status(500).send('Error interno del servidor!');
+});
+
+module.exports = server;
