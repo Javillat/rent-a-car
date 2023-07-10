7f779d639502f6d99f3ba37debc746880934b151
Javier Villatoro <wemakesoftx@gmail.com>
2023-07-10 00:25:15 -0600

    chore: Crear conexion a la db firestore

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,16 +1,18 @@
 const { initializeApp, applicationDefault, cert }  = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
+const { credential } = require("firebase-admin");
 
 const DATABASE_URL = process.env.DATABASE_URL;
 
-const firebaseConfig = {
-    databaseURL: "DATABASE_URL",
-};
+const serviceAccount = require("../src/config/rentacarvue-firebase-adminsdk-qf1hh-f473c0c142.json");
 
 //Inicializar firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp({
+    credential: credential.cert(serviceAccount),
+    databaseURL:DATABASE_URL,
+});
 
 //Inicializar base de datos en tiempo real y obtener la referencia al servicio.
-const db = getDatabase();
+const db = getFirestore();
 
 module.exports = db;
