d662518f8e49bdb485a7b0f68a54c2d278075ab7
Javier Villatoro <wemakesoftx@gmail.com>
2023-07-12 18:36:19 -0600

    chore: Agregar ruta obtener todos los autos

--- backend/src/routes/routescars.js
+++ backend/src/routes/routescars.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const cars = require('../controllers/cars.js');
 
-// Router for /api/v1/cars endpoint
+// Router for /cars/ endpoint
 routerCars = express.Router();
-routerCars.post('/newcar', cars.addCar);
-console.log('routercars');
 
-/* GET all available car models */  // Route: api/v1/cars (GET
-//routerCars.get('/', async function(req, res) {});
+routerCars.post('/newcar', cars.addCar);
+//console.log('routercars');
+routerCars.get('/getcars', cars.getCars);
 
 
 module.exports = routerCars;
