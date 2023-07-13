faf86e1174a7f74537e329df23a78c2901eb376d
Javier Villatoro <wemakesoftx@gmail.com>
2023-07-12 19:57:19 -0600

    feat: Agregar getCars, obtener todos los autos.

--- backend/src/controllers/cars.js
+++ backend/src/controllers/cars.js
@@ -2,29 +2,49 @@
 
 const db = require("../db");
 
-addCar = async(req, res) => {
+addCar = async (req, res) => {
 
     const carData = req.body;
     const docRef = db.collection('cars');
-    try{
+    try {
         console.log(carData);
-        if(carData) {
-            //await db.ref('cars').push(carData);
+        if (carData) {
             await docRef.add({
                 marca: carData.marca,
                 color: carData.color,
                 anyo: carData.anyo,
             });
-            res.status(201).json({ message: "Nuevo Carro Agregado"});
-        }else {
+            res.status(201).json({ message: "Nuevo Carro Agregado" });
+        } else {
             throw 'No hay datos en el body';
         }
 
-    }catch(error){
-        console.log('Error agregando el nuevo carro', error.message);
+    } catch (error) {
+        console.log('Error agregando el nuevo carro!', error.message);
     }
+}
+
+getCars = async (req, res) => {
+    let carsList = [];
+    //const carsSnapshot = db.collection('cars');
+    try {
+        const carsSnapshot = await db.collection('cars').get();
+        //for (let i=0 ;i<carsSnapshot.docs.length; i++){}
+        if (!carsSnapshot.empty) {
+            carsSnapshot.forEach(doc => {
+                carsList.push(doc.data()); 
+            });
+            console.log('Lista de autos ',carsList);    
+            return res.status(200).json(carsList)
+        } else {
+           return res.status(404).json('No existen autos');
+        };
+    } catch (error) {
+        console.log('Error interno del servidor!', error.message);
+    };
 }
 
 module.exports = {
     addCar,
+    getCars,
 }
