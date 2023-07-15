33a89de0461f8a7e2a4b75fe4448ddfd549d3c16
Javier Villatoro <wemakesoftx@gmail.com>
2023-07-15 17:43:47 -0600

    feat: Agregar modelo Car

---
+++ backend/src/models/car.js
@@ -0,0 +1,16 @@
+const admin = require('firebase-admin');
+
+const db = require('../db');
+
+Car = async(carData) => {
+    try{
+        autoRef = db.collection("cars").doc()
+        await autoRef.set(carData);
+        return autoRef.id; 
+
+    }catch(error){
+        console.error(error.message);
+    }
+}
+
+module.exports = Car;
