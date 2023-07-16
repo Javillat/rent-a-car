5694e13d3fb109c60a7a96fc9f8e07aec656e0cf
Javier Villatoro <wemakesoftx@gmail.com>
2023-07-15 19:29:17 -0600

    feat: Crear una instancia de User y pasarle los datos del body

---
+++ backend/src/controllers/usersfn.js
@@ -0,0 +1,26 @@
+
+const db = require("../db");
+const User = require("../models/user");
+
+/**
+ * Crear un nuevo usuario y guardarlo a firestore.
+ * Retornar un mensaje de error si el error ocurre.
+ */
+
+addUser = async (req, res) => {
+    
+    const userData = req.body;
+
+    try {
+        //Crea una nueva instancia del modelo user con los datos recibidos en req.body
+
+        if(Object.keys(userData).length === 0){
+            return res.sendStatus(204);
+        }else{
+            let newUser = await User({...userData});
+            return res.send(201, `Se creó el usuario con id ${newUser}, correctamente.`);
+        }
+    } catch(error){
+        console.error("Error al crear el usuario: ", error.message);
+    };
+}
