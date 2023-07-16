cf5eaf3f709f6e28d25131fb1173bd385bee276d
Javier Villatoro <wemakesoftx@gmail.com>
2023-07-15 21:20:22 -0600

    refactor: Quitar el spread operator a rentalData

--- backend/src/models/rental.js
+++ backend/src/models/rental.js
@@ -7,7 +7,7 @@ const db = require('../db');
 Rental = async(rentalData) => {
     try{
         rentalRef = db.collection("rental").doc();
-        await rentalRef.set(...rentalData);
+        await rentalRef.set(rentalData);
         return rentalData.id;
     }catch(error){
         console.error(error.message);
