c74f2d9dec9fd495492b378eced346fff7a13230
Javier Villatoro <wemakesoftx@gmail.com>
2023-07-21 10:42:29 -0600

    refactor: Cambiar metodo de login getUserByEmail

--- backend/src/controllers/usersfn.js
+++ backend/src/controllers/usersfn.js
@@ -1,6 +1,8 @@
 
-const firebase = require('firebase-admin');
+const admin = require('firebase-admin');
 const User = require("../models/user");
+const { getAuth } = require("firebase-admin/auth")
+//console.log(signInWithEmailAndPassword);
 
 
 /**
@@ -10,14 +12,20 @@ const User = require("../models/user");
 
 addUser = async (req, res) => {
 
-    const {email, password} = req.body;
+    const { email, password, phoneNumber, displayName, photoURL, disabled } = req.body;
 
     try {
-        if(email && password){
-            const credencial = await firebase.auth().createUser({email, password});
+        if (email && password) {
+            const credencial = await admin.auth().createUser({ 
+                email, 
+                password, 
+                phoneNumber,  
+                displayName,
+                disabled
+            });
             //console.log(credencial);
             res.sendStatus(201);
-        }else{
+        } else {
             res.status(204).send("Los campos no pueden estar vacios");
         }
     } catch (error) {
@@ -27,6 +35,35 @@ addUser = async (req, res) => {
     };
 }
 
+/**
+ * Hacer login al usuario y manejar errores. 
+ */
+
+signinUser = async (req, res) => {
+    const auth = getAuth();
+    console.log('Authentication ',auth);
+    const { email, password } = req.body;
+    console.log("Email y password ",email);
+    try {
+        if(email && password){
+            const signin = await auth.getUserByEmail(email, password);
+            console.log("Signin", signin);
+            //const token = await signin.user.getIdToken();
+            //console.log(`Usuario ${signin.user.displayName}, ha iniciado sesión, con token ${token}`);
+            res.sendStatus(200);
+
+        }else{
+            res.status(204).send("Los campos no pueden estar vacios");
+        }
+        
+    } catch (error) {
+        console.log(error.message, error.code);
+        return res.status(400).send(error);
+    }
+
+};
+
 module.exports = {
-    addUser
+    addUser,
+    signinUser,
 };
