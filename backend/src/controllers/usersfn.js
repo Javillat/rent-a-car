fcec8203c104620be87bfd5716fe84ddde88c49e
Javier Villatoro <wemakesoftx@gmail.com>
2023-07-22 15:30:28 -0600

    refactor: Inhabilitar la comprobacion de contrasena mediante bcrypt

--- backend/src/controllers/usersfn.js
+++ backend/src/controllers/usersfn.js
@@ -17,16 +17,16 @@ addUser = async (req, res) => {
         const { email, password, phoneNumber, displayName, photoURL, disabled } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
         if (email && password) {
-            const credencial = await admin.auth().createUser({ 
-                email, 
-                password: hashedPassword, 
-                phoneNumber,  
+            const credencial = await admin.auth().createUser({
+                email,
+                password: hashedPassword,
+                phoneNumber,
                 displayName,
                 photoURL: photoURL || "https://www.citypng.com/photo/20794/free-round-flat-male-portrait-avatar-user-icon-png",
                 disabled
             });
             //console.log(credencial);
-            res.status(201).json({message:"Usuario registrado satisfactoriamente con uid ", uid:credencial.uid});
+            res.status(201).json({ message: "Usuario registrado satisfactoriamente con uid", uid: credencial.uid });
         } else {
             res.status(204).send("Los campos no pueden estar vacios");
         }
@@ -42,26 +42,36 @@ addUser = async (req, res) => {
  */
 
 signinUser = async (req, res) => {
-    const auth = getAuth();
-    console.log('Authentication ',auth);
-    const { email, password } = req.body;
-    console.log("Email y password ",email);
+    // console.log('Authentication ',auth);
+    // console.log("Email y password ",email);
     try {
+        const auth = getAuth();
+        const { email, password } = req.body;
 
-        if(email && password){
-            const signin = await auth.getUserByEmail(email, password);
-            console.log("Signin", signin);
-            //const token = await signin.user.getIdToken();
-            //console.log(`Usuario ${signin.user.displayName}, ha iniciado sesión, con token ${token}`);
-            res.sendStatus(200);
+        const user = await auth.getUserByEmail(email, password);
+        console.log("Resultado user", user.uid)
+        //const isPasswordCorrect = await bcrypt.compare(password, user.passwordHash);
+        //console.log(isPasswordCorrect);
+        //.then(() => {
+        //console.log("Compare result ", isPasswordCorrect);
+        //if(!isPasswordCorrect){
+        const tokenUser = await auth.createCustomToken(user.uid);
+        //console.log('Token ', tokenUser);
+        return res.status(200).json({ tokenUser });
+        //return res.status(401).json({error: "Credenciales invalidas"});
+        //}else{
+    }
 
-        }else{
-            res.status(204).send("Los campos no pueden estar vacios");
-        }
-        
-    } catch (error) {
-        console.log(error.message, error.code);
-        return res.status(400).send(error);
+    //const token = await signin.user.getIdToken();
+    //console.log(`Usuario ${signin.user.displayName}, ha iniciado sesión, con token ${token}`);
+
+    // }else{
+    //     res.status(204).json({error:"Los campos no pueden estar vacios"});
+    // }
+
+    catch (error) {
+        //console.log(error.message, error.code);
+        return res.status(400).json({ Mensaje: error.message, Error: error.code });
     }
 
 };
