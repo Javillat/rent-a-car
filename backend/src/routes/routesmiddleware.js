46189b4157d8ef54b5a0955c9fdb0b956f6e21d3
Javier Villatoro <wemakesoftx@gmail.com>
2023-07-26 18:39:36 -0600

    chore: Agregar middleware authenticate a las rutas

--- backend/src/routes/routesmiddleware.js
+++ backend/src/routes/routesmiddleware.js
@@ -8,11 +8,11 @@ const rental = require("../controllers/rentalfn.js");
 routerMiddleware = express.Router();
 
 //middleware cars
-routerMiddleware.post('/newcar', cars.addCar);          // Agregar un nuevo auto
-routerMiddleware.get('/getcars', cars.getCars);         // Obtener todos los autos
-routerMiddleware.get('/getcar/:id', cars.getCarById);   // Obtener un auto por su Id
-routerMiddleware.delete('/delete/:id', cars.deleteCar); // Eliminar un auto
-routerMiddleware.put('/update/:id', cars.updateCar);    // Actualizar un auto
+routerMiddleware.post('/newcar', users.authenticate, cars.addCar);          // Agregar un nuevo auto
+routerMiddleware.get('/getcars', users.authenticate, cars.getCars);         // Obtener todos los autos
+routerMiddleware.get('/getcar/:id', users.authenticate, cars.getCarById);   // Obtener un auto por su Id
+routerMiddleware.delete('/delete/:id', users.authenticate, cars.deleteCar); // Eliminar un auto
+routerMiddleware.put('/update/:id', users.authenticate, cars.updateCar);    // Actualizar un auto
 
 //middleware users
 routerMiddleware.post('/signup', users.addUser);   // Registrar usuario
@@ -22,7 +22,7 @@ routerMiddleware.put('/logout', users.signoutUser);// Desloguear usuario
 //middleware rental
 routerMiddleware.post('/:carId', users.authenticate, rental.addRentals);                // Agregar una renta
 routerMiddleware.post('/return/:rentalId', users.authenticate, rental.addReturnCar);    // Rerornar un auto
-routerMiddleware.get('/allrental', rental.getRentalList);                               // Obtener autos rentados
+routerMiddleware.get('/allrental', users.authenticate, rental.getRentalList);           // Obtener autos rentados
 
 
 module.exports = routerMiddleware;
