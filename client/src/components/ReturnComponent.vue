77b7fd54651d3181fc6a5592fdf3ffba77fb3487
Javier Villatoro <wemakesoftx@gmail.com>
2023-07-30 19:06:57 -0600

    feat: Componente return template

---
+++ client/src/components/ReturnComponent.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h2>Confirmar devolucion</h2>
+        <p>{{ rental.car.marca }} - {{ rental.car.modelo }} (Año {{ rental.car.anyo }})</p>
+        <button @click="confirmReturn">Confirmar devolucion</button>
+    </div>
+</template>
