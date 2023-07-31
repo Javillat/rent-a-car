47c4e9127802a0aabb42a86be7b409a7e49404c5
Javier Villatoro <wemakesoftx@gmail.com>
2023-07-31 09:29:34 -0600

    fix: Cambiar state.idToken
    Eliminar el codigo comentado, pasado a index

--- client/src/main.js
+++ client/src/main.js
@@ -7,15 +7,6 @@ import VueRouter from './router';
 import axios from 'axios';
 import Vuex from 'vuex';
 
-// // import RegisterComponent from './components/RegisterComponent.vue';
-// import LoginComponent from './components/LoginComponent.vue';
-// // import LogoutComponent from './components/LogoutComponent.vue';
-// import HomeComponent from './components/HomeComponent.vue';
-// import CarsComponent from './components/CarsComponent.vue';
-// // import CarDetailsComponent from './components/CarDetailsComponent.vue';
-// import RentalComponent from './components/RentalComponent.vue';
-// import ReturnComponent from './components/ReturnComponent.vue';
-
 Vue.config.productionTip = false
 
 // Vue.use(VueRouter);
@@ -25,21 +16,6 @@ Vue.prototype.$http = axios.create({
   baseURL: 'http://localhost:5000'
 });
 
-// const routes = [
-//   {path: '/', component: HomeComponent},
-//   {path: '/login', component: LoginComponent},
-//   // {path: '/register', component: RegisterComponent},
-//   {path: '/car', component: CarsComponent},
-//   // {path: '/car/detail', component: CarDetailsComponent},
-//   {path: '/rent/:carId', component: RentalComponent},
-//   {path: '/return/:rentalId', component: ReturnComponent}
-//   // {path: '/logout', component: LogoutComponent},
-// ];
-
-// const router = new VueRouter({
-//   routes
-// });
-
 const store = new Vuex.Store({
   state: {
     isLoggedIn: false,
@@ -48,7 +24,7 @@ const store = new Vuex.Store({
   mutations: {
     SETH_AUTH (state, payload) {
       state.isLoggedIn = payload.isLoggedIn;
-      state.idToken = payload.iDToken;
+      state.idToken = payload.idToken;
     }
   }
 });
