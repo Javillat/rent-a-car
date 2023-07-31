3ae8597060c97fbb93e4f113ebafebea14055cc9
Javier Villatoro <wemakesoftx@gmail.com>
2023-07-31 12:49:33 -0600

    refactor: Redireccionar a login o register links

--- client/src/components/HomeComponent.vue
+++ client/src/components/HomeComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Bienvenido al alquiler de autos</h1>
-        <p v-if="!isLoggedIn">Por favor inicia sesion para continuar</p>
+        <p v-if="!isLoggedIn">Por favor, <router-link to="/login">inicia sesión</router-link> o <router-link to="/register">regístrate</router-link> para continuar</p>
     </div>
 </template>
 
