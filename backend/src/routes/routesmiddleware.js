cf7b6b6a3c5d9887586e4f785928e4b27e106a4a
Javier Villatoro <wemakesoftx@gmail.com>
2023-08-01 15:06:09 -0600

    chore: Agregar ruta getRentByUser auth/:userId

--- backend/src/routes/routesmiddleware.js
+++ backend/src/routes/routesmiddleware.js
@@ -20,7 +20,8 @@ routerMiddleware.post('/login', users.signinUser); // loguear usuario
 routerMiddleware.put('/logout', users.signoutUser);// Desloguear usuario
 
 //middleware rental
-routerMiddleware.post('/:carId', users.authenticate, rental.addRentals);                // Agregar una renta
+routerMiddleware.post('/:carId', users.authenticate, rental.addRentals); 
+routerMiddleware.get('/auth/:userId', users.authenticate, rental.getRentByUser);               // Agregar una renta
 routerMiddleware.post('/return/:rentalId', users.authenticate, rental.addReturnCar);    // Rerornar un auto
 routerMiddleware.get('/allrental', users.authenticate, rental.getRentalList);           // Obtener autos rentados
 
