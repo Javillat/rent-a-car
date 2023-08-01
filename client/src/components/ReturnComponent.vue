74ec926bb590cbb7f86c2e799bb04d8cbd0c4114
Javier Villatoro <wemakesoftx@gmail.com>
2023-08-01 11:02:50 -0600

    fix: Cambiar parametro en localStorage

--- client/src/components/ReturnComponent.vue
+++ client/src/components/ReturnComponent.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
         <h2>Confirmar devolucion</h2>
-        <p>{{ rental.car.marca }}</p>
-        <p>{{ rental.car.tipo }}</p>
-        <p>(Año {{ rental.car.año }})</p>
-        <button @click="confirmReturn">Confirmar devolución</button>
+        <ul>
+            <li v-for="rent in cars" :key="rent.id">
+            <p>{{ rent.marca }}</p>
+            <p>{{ rent.tipo }}</p>
+            <p>(Año {{ rent.año }})</p>
+            <button @click="confirmReturn">Confirmar devolución</button>
+        </li></ul>
     </div>
 </template>
 
@@ -18,7 +21,7 @@ export default {
   methods: {
     confirmReturn () {
       // Obtener el idToken para incluirlo en la solicitud
-      const idToken = localStorage.getItem('idToken')
+      const idToken = localStorage.getItem('token')
       this.$http
         .post(`/rental/return/${this.$route.params.rentalId}`, null, {
           headers: {'Authorization': `Bearer ${idToken}`}
@@ -42,22 +45,22 @@ export default {
           console.error(error)
           alert('Error al obtener informacion del alquiler!.')
         })
+    },
+    fetchRentedCars () {
+      const idToken = localStorage.getItem('token')
+      console.log(idToken)
+      this.$http
+        .get('rental/allrental', {
+          headers: {'Authorization': `Bearer ${idToken}`}
+        })
+        .then((response) => {
+          this.cars = response.data
+          alert('Listado de autos rentados!')
+        })
+        .catch((error) => {
+          console.log('Error consultando los autos rentados:', error)
+        })
     }
-  },
-  fetchRentedCars () {
-    const idToken = localStorage.getItem('idToken')
-
-    this.$http
-      .get('rental/allrental', {
-        headers: {'Authorization': `Bearer ${idToken}`}
-      })
-      .then((response) => {
-        this.cars = response.data
-        alert('Listado de autos rentados!')
-      })
-      .catch((error) => {
-        console.log('Error consultando los autos rentados:', error)
-      })
   },
   mounted () {
     this.fetchRentedCars()
