7531e237a51b89d4f9f353a9b21b1dad8bba92fd
Javier Villatoro <wemakesoftx@gmail.com>
2023-08-01 20:09:23 -0600

    feat: Crear Navbar

---
+++ client/src/components/NavComponent.vue
@@ -0,0 +1,98 @@
+<!-- eslint-disable indent -->
+<template>
+    <div>
+        <div class="nave-menu">
+            <i class="fas fa-bars" @click="showMenu()"></i>
+            <div class="nav-content" :class="this.showMobileMenu ? 'open-menu' : 'closed-menu'">
+                <div class="logo">Logo</div>
+                <div class="nav-items">
+                    <div><router-link to="/car">Autos disponibles</router-link></div>
+                    <div><router-link to="/rent">Autos rentados</router-link></div>
+                </div>
+                <div class="login-button">Login</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      showMobileMenu: false
+    }
+  },
+  methods: {
+    showMenu () {
+      this.showMobileMenu = !this.showMobileMenu
+    }
+  }
+}
+</script>
+
+<style>
+.nav-menu {
+    background-color: white;
+}
+
+.nav-content {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 30px;
+    align-items: center;
+}
+
+.nav-items {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+
+    li {
+        padding: 0 10px;
+    }
+}
+
+i {
+    display: none;
+}
+
+// Mobile version - hidden hamburger menu
+@media screen and (max-width: 768px) {
+    .nav-menu {
+        padding-top: 10px;
+        position: absolute;
+        width: 100%;
+    }
+
+    .open-menu {
+        opacity: 1;
+        height: 150px;
+    }
+
+    .closed-menu {
+        opacity: 0;
+        height: 0;
+        padding: 0;
+    }
+
+    .nav-content {
+        flex-direction: column;
+        z-index: 100;
+        position: relative;
+        transition: all 0.2s ease-out;
+    }
+
+    .nav-items {
+        flex-direction: column;
+    }
+
+    i {
+        display: block;
+        text-align: right;
+        padding: 0 10px 10px 0;
+    }
+}
+</style>
